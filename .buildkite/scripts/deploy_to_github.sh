2d09c3113d8b0d4757f23126e7d40d1fa0055ad1
Alex Chan <alex@alexwlchan.net>
2022-04-19 12:19:41 +0100

    try building a proper docker image, provide the git config

--- .buildkite/scripts/deploy_to_github.sh
+++ .buildkite/scripts/deploy_to_github.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
+# This deploys a collection of built HTML files to GitHub.
+#
+# Because this involves Git, we can't use the Alpine Node image
+# we do to build the HTML -- it doesn't have Git installed.
+# We have to build a custom image.
 
 set -o errexit
 set -o nounset
 set -o verbose
 
-NODE_IMAGE="760097843905.dkr.ecr.eu-west-1.amazonaws.com/node:16-alpine"
 ROOT=$(git rev-parse --show-toplevel)
+DEPLOY_IMAGE="node_with_git"
+
+docker build \
+  --tag "$DEPLOY_IMAGE" \
+  --file "$ROOT/.buildkite/scripts/deploy.Dockerfile" .
 
 docker run --rm --tty \
   --volume "$ROOT:$ROOT" \
+  --volume ~/.ssh:/root/.ssh \
+  --volume ~/.gitconfig:/root/.gitconfig \
   --workdir "$ROOT" \
   --env SSH=true \
-  "$NODE_IMAGE" yarn deploy
+  "$DEPLOY_IMAGE" yarn deploy
