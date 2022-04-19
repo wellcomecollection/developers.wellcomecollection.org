3a5d9f9031c8520154db89d79f6346cc90437f32
Jamie Parkinson <j.parkinson@wellcome.ac.uk>
2022-04-19 13:03:40 +0100

    Syntax, again

--- .buildkite/scripts/deploy_to_github.sh
+++ .buildkite/scripts/deploy_to_github.sh
@@ -33,7 +33,7 @@ docker run --rm --tty \
 docker run --rm --tty \
   --volume "$ROOT:$ROOT" \
   --volume ~/.gitconfig:/root/.gitconfig \
-  --volume ~/.ssh:/root/.ssh
+  --volume ~/.ssh:/root/.ssh \
   --volume $SSH_AUTH_SOCK:/ssh-agent \
   --env SSH_AUTH_SOCK=/ssh-agent \
   --workdir "$ROOT" \
