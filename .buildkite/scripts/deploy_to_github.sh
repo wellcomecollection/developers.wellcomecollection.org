31545bdef18d3ec811887234e52969311073742e
Alex Chan <alex@alexwlchan.net>
2022-04-19 12:04:19 +0100

    Go as far as building the HTML, but not publishing

--- .buildkite/scripts/deploy_to_github.sh
+++ .buildkite/scripts/deploy_to_github.sh
@@ -2,7 +2,17 @@
 
 set -o errexit
 set -o nounset
+set -o verbose
 
+NODE_IMAGE="760097843905.dkr.ecr.eu-west-1.amazonaws.com/node:16-alpine"
 ROOT=$(git rev-parse --show-toplevel)
 
-echo "$ROOT"
+docker run --rm --tty \
+  --volume "$ROOT:$ROOT" \
+  --workdir "$ROOT" \
+  "$NODE_IMAGE" yarn
+
+docker run --rm --tty \
+  --volume "$ROOT:$ROOT" \
+  --workdir "$ROOT" \
+  "$NODE_IMAGE" yarn build
