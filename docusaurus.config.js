0de5055400ddd6f745db2899286eabdfdb7e0e66
harrisonpim <hello@harrisonpim.com>
2022-05-09 11:52:56 +0100

    delete links to a misisng getting-started page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,61 +1,59 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Developers | Wellcome Collection',
-  tagline: 'Make something new with our collections',
-  url: 'https://developers.wellcomecollection.org',
-  baseUrl: '/',
-  favicon: '/icons/favicon.ico',
+  title: "Developers | Wellcome Collection",
+  tagline: "Make something new with our collections",
+  url: "https://developers.wellcomecollection.org",
+  baseUrl: "/",
+  favicon: "/icons/favicon.ico",
   trailingSlash: false,
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  organizationName: 'wellcomecollection',
-  projectName: 'developers.wellcomecollection.org',
-  deploymentBranch: 'gh-pages',
-  
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  organizationName: "wellcomecollection",
+  projectName: "developers.wellcomecollection.org",
+  deploymentBranch: "gh-pages",
+
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          remarkPlugins: [
-            require('mdx-mermaid'),
-          ],
+          sidebarPath: require.resolve("./sidebars.js"),
+          remarkPlugins: [require("mdx-mermaid")],
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
     [
-      'redocusaurus',
+      "redocusaurus",
       {
         specs: [
           {
             routePath: "api/catalogue",
-            spec: 'reference/catalogue.yaml',
-            layout: { title: 'Catalogue API' }
+            spec: "reference/catalogue.yaml",
+            layout: { title: "Catalogue API" },
           },
           {
             routePath: "api/iiif",
-            spec: 'reference/iiif.yaml',
-            layout: { title: 'IIIF APIs' }
+            spec: "reference/iiif.yaml",
+            layout: { title: "IIIF APIs" },
           },
           {
             routePath: "api/text",
-            spec: 'reference/text.yaml',
-            layout: { title: 'Text API' }
+            spec: "reference/text.yaml",
+            layout: { title: "Text API" },
           },
         ],
         theme: {
-          primaryColor: '#007868',
+          primaryColor: "#007868",
         },
       },
     ],
@@ -64,112 +62,103 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      image: 'icons/android-192x192.png',
+      image: "icons/android-192x192.png",
       navbar: {
-        title: 'Developers',
+        title: "Developers",
         hideOnScroll: true,
         logo: {
-          alt: 'Wellcome Collection',
-          src: 'images/wellcome-collection-black.svg',
-          srcDark: 'images/wellcome-collection-white.svg',
+          alt: "Wellcome Collection",
+          src: "images/wellcome-collection-black.svg",
+          srcDark: "images/wellcome-collection-white.svg",
         },
         items: [
           {
-            to: 'docs',
-            position: 'left',
-            label: 'Getting Started',
-          },
-          {
-            type: 'dropdown',
-            to: 'docs/api',
-            position: 'left',
-            label: 'API Reference',
+            type: "dropdown",
+            to: "docs/api",
+            position: "left",
+            label: "API Reference",
             items: [
               {
-                to: 'api/catalogue',
-                label: 'Catalogue',
+                to: "api/catalogue",
+                label: "Catalogue",
               },
               {
-                to: 'api/iiif',
-                label: 'IIIF',
+                to: "api/iiif",
+                label: "IIIF",
               },
               {
-                to: 'api/text',
-                label: 'Text',
+                to: "api/text",
+                label: "Text",
               },
             ],
           },
           {
-            href: 'https://github.com/wellcomecollection',
-            position: 'right',
-            label: 'GitHub',
+            href: "https://github.com/wellcomecollection",
+            position: "right",
+            label: "GitHub",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Documentation',
+            title: "Documentation",
             items: [
               {
-                label: 'Getting Started',
-                to: 'docs',
-              },
-              {
-                label: 'API Reference',
-                to: 'docs/api',
+                label: "API Reference",
+                to: "docs/api",
               },
             ],
           },
           {
-            title: 'Get Involved',
+            title: "Get Involved",
             items: [
               {
-                label: 'Stacks',
-                href: 'https://stacks.wellcomecollection.org',
+                label: "Stacks",
+                href: "https://stacks.wellcomecollection.org",
               },
               {
-                label: 'Roadmap',
-                href: 'https://roadmap.wellcomecollection.org',
+                label: "Roadmap",
+                href: "https://roadmap.wellcomecollection.org",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/wellcomecollection',
+                label: "GitHub",
+                href: "https://github.com/wellcomecollection",
               },
             ],
           },
           {
-            title: 'Contact Us',
+            title: "Contact Us",
             items: [
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/ExploreWellcome',
+                label: "Twitter",
+                href: "https://twitter.com/ExploreWellcome",
               },
               {
-                label: 'Facebook',
-                href: 'https://www.facebook.com/wellcomecollection',
+                label: "Facebook",
+                href: "https://www.facebook.com/wellcomecollection",
               },
               {
-                label: 'Email',
-                href: 'mailto:digital@wellcomecollection.org',
+                label: "Email",
+                href: "mailto:digital@wellcomecollection.org",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'Jobs',
-                href: 'https://wellcome.ac.uk/jobs',
+                label: "Jobs",
+                href: "https://wellcome.ac.uk/jobs",
               },
               {
-                label: 'Privacy',
-                href: 'https://wellcome.org/who-we-are/privacy-and-terms',
+                label: "Privacy",
+                href: "https://wellcome.org/who-we-are/privacy-and-terms",
               },
               {
-                label: 'Wellcome Collection',
-                href: 'https://wellcomecollection.org',
+                label: "Wellcome Collection",
+                href: "https://wellcomecollection.org",
               },
             ],
           },
@@ -182,7 +171,7 @@ const config = {
       },
     }),
 
-    plugins: [],
+  plugins: [],
 };
 
 module.exports = config;
