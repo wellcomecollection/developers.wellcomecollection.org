eee2829b9b0742da538a98f43829aa2d4c39463a
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-09-01 15:38:28 +0000

    Deploy website - based on a50e1de6c8e674d84f2b9c5bcd7baa241c11d19e

---
+++ assets/js/5801a87c.a8b462ef.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdevelopers=self.webpackChunkdevelopers||[]).push([[694],{2029:function(e){e.exports={title:"Text API"}}}]);
--- assets/js/a6b0227b.2a58c668.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdevelopers=self.webpackChunkdevelopers||[]).push([[571],{8385:function(e){e.exports=JSON.parse('{"title":"IIIF APIs"}')}}]);
